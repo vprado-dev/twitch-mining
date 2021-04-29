82cf685cfe12402c97023caef741babe7013241d
Vinicius Prado <vinicavagna@gmail.com>
2021-04-29 16:05:33 -0300

    :sparkles: get specific tab

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,10 @@
 chrome.runtime.onMessage.addListener(request => {
-  console.log('Message received in background.js!', request);
+  chrome.tabs.query({ url: 'https://www.twitch.tv/*/' }, result => {
+    if (result.length <= 0) {
+      console.log('Não há nenhuma guia com um canal da twitch aberto');
+      return;
+    } else {
+      console.log('foi');
+    }
+  });
 });
